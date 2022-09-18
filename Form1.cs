4382548754519ad84f4c63512d5809f086779f89
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-09-19 06:29:25 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ui042
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Enterキーで押された
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("OKボタンがクリックされました");
+        }
+
+        /// <summary>
+        /// ESCキーで押された
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("キャンセルボタンがクリックされました");
+        }
+    }
+}
